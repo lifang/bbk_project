59a5734dbbd4a54dc88726c5cfea0431fda7f3be
chenlong <KLoong777@163.com>
2013-12-09 16:59:36 +0800

    cl

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,12 +54,14 @@ class UsersController < ApplicationController
         complet_count = Task.find_by_sql("select count(*) count from tasks where tasks.`status` in (#{Task::STATUS[:WAIT_FINAL_CHECK]},#{Task::STATUS[:FINAL_CHECK_COMPLETE]}) and tasks.task_tag_id=#{task_tag_id}")
         unfinish_count = Task.find_by_sql("select count(*) count from tasks where tasks.`status` not in (#{Task::STATUS[:WAIT_FINAL_CHECK]},#{Task::STATUS[:FINAL_CHECK_COMPLETE]}) and tasks.task_tag_id=#{task_tag_id}")
         task_tags_list = task_tag.attributes
+        flash_count = Task.find_by_sql("select count(*) count from tasks where task_tag_id = #{task_tag.id} and is_upload_source = 1")
         task_tags_list[:id] = task_tag.id
         task_tags_list[:name] = task_tag.name
         task_tags_list[:created_at] = task_tag.created_at.strftime("%F-%H")
         task_tags_list[:complet_count] = complet_count[0].count
         task_tags_list[:unfinish_count] = unfinish_count[0].count
         task_tags_list[:status] = task_tag.status
+        task_tags_list[:flash_count] = flash_count[0].count
         @task_tags_arr << task_tags_list
       end
     else
